954b8e20254157bb588dc58f691d1e8b04f9a1a6
Winicius-Neves <winiciusneves2004@hotmail.com>
2024-06-05 09:42:44 -0300

    create back-end

---
+++ backend/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = require('./router');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.json());
+
+app.use(router);
+
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}`, `http://localhost:${port}`);
+});
