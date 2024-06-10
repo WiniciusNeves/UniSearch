2aece384bf54d38ea1905745630f57b0421d8439
AndreMour <andreluizmoura2004@outlook.com>
2024-06-10 08:50:23 -0300

    Add toast´s

--- frontend/src/pages/Auth/index.js
+++ frontend/src/pages/Auth/index.js
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
-import { Image, Alert } from "react-native";
+import { Image, View } from "react-native";
 import { ImageView, Body, Input, ForgotText, Button, ButtonText, Text, RegisterButon } from "./styles";
 import { useNavigation } from "@react-navigation/native";
+import Toast, { BaseToast } from 'react-native-toast-message';
 import { BackButton } from "../MenuResgister/styles";
 import { Arrow } from "../Suporte/styles";
 import api from '../../api/api';
@@ -12,22 +13,54 @@ const Auth = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
+    const toastConfig = {
+        error: ({ text1, text2 }) => (
+          <BaseToast
+            {...{ text1, text2 }}
+            style={{ backgroundColor: '#fff', borderLeftColor: 'red' }}
+            textStyle={{ color: '#909090' }}
+          />
+        ),
+      };
+    
+    const toastVerify = () => {
+        Toast.show({
+          type: "error",
+          text1: "Credenciais inválidas.",
+          text2: "Por favor, verifique seu e-mail e senha.",
+          autoHide: true,
+          visibilityTime: 2500,
+        })
+      }
+    
+    const toastRetry = () => {
+        Toast.show({
+          type: "error",
+          text1: "Erro ao acessar sua conta.",
+          text2: "Por favor, tente novamente mais tarde.",
+          autoHide: true,
+          visibilityTime: 2500,
+        })
+      }
+
     const handleLogin = async () => {
         try {
             const response = await api.post('/auth/login', { email, password });
             if (response.status === 200) {
                 navigation.navigate('Feed', { user: response.data });
             } else {
-                Alert.alert('Erro', 'Credenciais inválidas. Por favor, verifique seu e-mail e senha.');
+                toastVerify();
             }
         } catch (error) {
-            Alert.alert('Erro', 'Erro ao acessar sua conta. Por favor, tente novamente mais tarde.');
+            toastRetry();
         }
     };
 
-
     return (
         <>
+            <View style={{ zIndex: 9999 }}>
+                <Toast config={toastConfig} />
+            </View>
             <BackButton onPress={() => navigation.goBack()}>
                 <Arrow name="arrowleft" />
             </BackButton>
