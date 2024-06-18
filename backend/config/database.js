7d5bb390bb8403d898d3e78b77ebe2f2a7340833
AndreMour <andreluizmoura2004@outlook.com>
2024-06-18 19:45:24 -0300

    Change password

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,9 +2,9 @@ require('dotenv').config();
 const { Sequelize, DataTypes } = require('sequelize');
 
 
-const dbName = process.env.DB_NAME || 'unisearch';
+const dbName = process.env.DB_NAME;
 const dbUser = process.env.DB_USER || 'root';
-const dbPassword = process.env.DB_PASSWORD || '123';
+const dbPassword = process.env.DB_PASSWORD || 'Andre4584';
 const dbHost = process.env.DB_HOST || 'localhost';
 const dbPort = process.env.DB_PORT || 3306;
 
